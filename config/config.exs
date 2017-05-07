0c41f1c11b72c88179151906035bf52d54462677
patientplatypus <pweyand@gmail.com>
2017-05-07 11:26:59 -0500

    up to page 91

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :rumbl,
+  ecto_repos: [Rumbl.Repo]
+
+# Configures the endpoint
+config :rumbl, Rumbl.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "n/M4tNzDKYK3puGB50R88kzh2xrC5nZZ2WSAN36pM9G8huJ45B+fDPZ86mnbqi4q",
+  render_errors: [view: Rumbl.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Rumbl.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ web/controllers/app_controller.ex
@@ -0,0 +1,8 @@
+defmodule Rumbl.AppController do
+  use Rumbl.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+end
